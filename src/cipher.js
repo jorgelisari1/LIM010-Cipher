6b6f2feb5cf8850c7ac9e2fe6093d29a3089a409
jorgelisari1 <jorgelis_ari@hotmail.com>
2019-06-17 09:19:43 -0500

    carg

--- src/cipher.js
+++ src/cipher.js
@@ -2,54 +2,49 @@
 window.cipher = {
   encode: (offset, string) => {
 
-    let valorascii,result,newletter,stringResult ="", cont =0;
-      if(offset> 0 && string.length>=0)
-      {
-        do{
-          if(string.charCodeAt(cont) > 90 || string.charCodeAt(cont)<65)
-          {
-            stringResult = stringResult + string.charAt(cont);
-          }else{
-            valorascii = string.charCodeAt(cont);
-            result = (valorascii-65+(parseInt(offset,10)))%26+65;
-            newletter = String.fromCharCode(result);
-            stringResult = stringResult + newletter;
-          }
-          cont = cont +1;
-        }while (cont <string.length);
-          return stringResult;
+    let valorascii, result, newletter, stringResult = "", cont = 0;
+    if (offset > 0 && string.length >= 0) {
+      do {
+        if (string.charCodeAt(cont) > 90 || string.charCodeAt(cont) < 65) {
+          stringResult = stringResult + string.charAt(cont);
+        } else {
+          valorascii = string.charCodeAt(cont);
+          result = (valorascii - 65 + (parseInt(offset, 10))) % 26 + 65;
+          newletter = String.fromCharCode(result);
+          stringResult = stringResult + newletter;
+        }
+        cont = cont + 1;
+      } while (cont < string.length);
+      return stringResult;
 
-      }
+    }
   },
   decode: (offset, string) => {
-    let valorascii,result,newletter,stringResult ="", cont =0;
-      if(offset> 0 && string.length>=0)
-      {
-        do{
-          if(string.charCodeAt(cont) > 90 || string.charCodeAt(cont)<65)
-          {
-            stringResult = stringResult + string.charAt(cont);
-          }else{
+    let valorascii, result, newletter, stringResult = "", cont = 0;
+    if (offset > 0 && string.length >= 0) {
+      do {
+        if (string.charCodeAt(cont) > 90 || string.charCodeAt(cont) < 65) {
+          stringResult = stringResult + string.charAt(cont);
+        } else {
 
-            while (offset>26) {
-              offset = offset-26;
+          while (offset > 26) {
+            offset = offset - 26;
 
-            }
-            valorascii = string.charCodeAt(cont);
-            result = (valorascii-65-(parseInt(offset,10)))
-            if(result< 0)
-            {
-              result = result+26;
-            }
-            result = result +65;
-            newletter = String.fromCharCode(result);
-            //alert(newletter);
-            stringResult = stringResult + newletter;
           }
-          cont = cont +1;
-        }while (cont <string.length);
-          return stringResult;
+          valorascii = string.charCodeAt(cont);
+          result = (valorascii - 65 - (parseInt(offset, 10)))
+          if (result < 0) {
+            result = result + 26;
+          }
+          result = result + 65;
+          newletter = String.fromCharCode(result);
+          //alert(newletter);
+          stringResult = stringResult + newletter;
+        }
+        cont = cont + 1;
+      } while (cont < string.length);
+      return stringResult;
 
-      }
     }
+  }
 };
