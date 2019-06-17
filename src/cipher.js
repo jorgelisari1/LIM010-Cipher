a491901c15eb53cc06754bbd749b9db43affe6b0
jorgelisari1 <jorgelis_ari@hotmail.com>
2019-06-17 10:34:22 -0500

    cipher love para negativos

--- src/cipher.js
+++ src/cipher.js
@@ -3,7 +3,6 @@ window.cipher = {
   encode: (offset, string) => {
 
     let valorascii, result, newletter, stringResult = "", cont = 0;
-    if (offset > 0 && string.length >= 0) {
       do {
         if (string.charCodeAt(cont) > 90 || string.charCodeAt(cont) < 65) {
           stringResult = stringResult + string.charAt(cont);
@@ -17,11 +16,9 @@ window.cipher = {
       } while (cont < string.length);
       return stringResult;
 
-    }
   },
   decode: (offset, string) => {
     let valorascii, result, newletter, stringResult = "", cont = 0;
-    if (offset > 0 && string.length >= 0) {
       do {
         if (string.charCodeAt(cont) > 90 || string.charCodeAt(cont) < 65) {
           stringResult = stringResult + string.charAt(cont);
@@ -29,7 +26,6 @@ window.cipher = {
 
           while (offset > 26) {
             offset = offset - 26;
-
           }
           valorascii = string.charCodeAt(cont);
           result = (valorascii - 65 - (parseInt(offset, 10)))
@@ -40,11 +36,9 @@ window.cipher = {
           newletter = String.fromCharCode(result);
           //alert(newletter);
           stringResult = stringResult + newletter;
-        }
+      }
         cont = cont + 1;
-      } while (cont < string.length);
+    } while (cont < string.length);
       return stringResult;
 
-    }
-  }
-};
+}};
